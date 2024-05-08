bac5d07aeecc3e6d1a0716e58bc2b61757630ede
HodayaHH <chahodaya1@gmail.com>
2024-05-08 18:37:08 +0300

    primitives

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,6 +1,8 @@
 package primitives;
 
+// class representing a vector
 public class Vector extends Point {
+    //Parameter constructor
     public Vector(double x, double y, double z) {
         super(x, y, z);
         if (xyz.equals(Double3.ZERO) )
@@ -9,7 +11,7 @@ public class Vector extends Point {
         }
     }
 
-
+    // Parameter constructor
     public Vector(Double3 double3) {
         super(double3);
         if (double3.equals(Double3.ZERO))
@@ -19,14 +21,43 @@ public class Vector extends Point {
 
     }
 
+    //adding of 2 vectors (returns a new vector)
     public Vector add(Vector v1) {
         return new Vector(xyz.add(v1.xyz));
     }
 
-//    public Vector  scale (Double3 p1){
-//        return new Vector()
-//
-//    }
+    //Multiplies a vector by a number - a scalar (returns a new vector)
+    public Vector  scale (double rhs){
+        return new Vector(xyz.scale(rhs));
+    }
 
+    //function that multiplies a vector by a number
+    public double dotProduct(Vector v1){
+        return this.xyz.d1*v1.xyz.d1 +this.xyz.d2*v1.xyz.d2 +this.xyz.d3*v1.xyz.d3;
+    }
 
+    //function that calculates a vector product
+    public Vector crossProduct (Vector v1) {
+        return new Vector(this.xyz.d1*v1.xyz.d2-this.xyz.d2*v1.xyz.d1 ,
+                           this.xyz.d1*v1.xyz.d3-this.xyz.d3*v1.xyz.d1 ,
+                           this.xyz.d2*v1.xyz.d3-this.xyz.d3*v1.xyz.d2);
+
+    }
+
+    // Calculation of the squared length of the vector - (|vector|^2) = vector^2
+    public double lengthSquared(){
+        return xyz.d1*xyz.d1+xyz.d2*xyz.d2+xyz.d3*xyz.d3;
+    }
+
+    //Calculate the length of the vector
+    public double length(){
+        return Math.sqrt(lengthSquared());
+    }
+
+    //A normalization method that returns a new normalized vector - (a unit vector in the same direction as
+    // the original vector)
+    public Vector normalize(){
+        double l1= length();
+        return  new Vector(xyz.d1/l1,xyz.d2/l1,xyz.d3/l1);
+    }
 }
