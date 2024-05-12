9a29e315a9fdf348255e8827a4cdc6a15caefa4b
HodayaHH <chahodaya1@gmail.com>
2024-05-12 14:11:19 +0300

    שו"ש - equals

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -67,6 +67,14 @@ public class Vector extends Point {
                 '}';
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        return (obj instanceof Vector other)
+               && super.equals(other);
+    }
+
+
 
 
 }
