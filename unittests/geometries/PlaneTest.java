7f065dbac174cd942751e18bb900ad49262f48cf
HodayaHH <chahodaya1@gmail.com>
2024-05-23 23:07:02 +0300

    unit test - for plan

--- unittests/geometries/PlaneTest.java
+++ unittests/geometries/PlaneTest.java
@@ -1,12 +1,26 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlaneTest {
 
     @Test
-    void testGetNormal() {
+    void testGetNormal( ) {
+        Point p1 = new Point(0, 0, 0);
+        Point p2 = new Point(0, 0, 1);
+        Point p3 = new Point(0, 1, 0);
+        Plane plane = new Plane(p1,p2,p3);
+        Vector v1= p2.subtract(p1);
+        Vector v2= p3.subtract(p1);
+        Vector normal = v1.crossProduct(v2).normalize();
+        assertEquals(normal, plane.getNormal());
+        // The normal vector of the plane should be the same as the given normal vector
+
     }
+
+
 }
